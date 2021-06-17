06095d124a2627f842806ccf1e826c935b89b89b
Louis Lambeau <llambeau@quadrabee.com>
2021-06-17 18:56:05 +0200

    harpocrates setup takes a --hub flag.

--- src/commands/setup.js
+++ src/commands/setup.js
@@ -5,9 +5,9 @@ const setup = require('../core/setup');
 class SetupCommand extends Command {
   async run() {
     const { flags } = this.parse(SetupCommand);
-    const { force, username } = flags;
+    const { force, username, hub } = flags;
     try {
-      setup({ username }, force);
+      setup({ username, hub }, force);
     } catch (err) {
       if (err instanceof AlreadySetupError) {
         this.error('Harpocrates is already configured, if you want to regenerate a new key pair use --force');
@@ -24,6 +24,7 @@ This command initializes harpocrates, creating a new rsa pair for encryption of 
 
 SetupCommand.flags = {
   username: flags.string({ char: 'u', description: 'your username', required: true }),
+  hub: flags.string({ char: 'h', description: 'the address of the harpocates hub', required: true }),
   force: flags.boolean({ char: 'f', default: false, description: 'forces the setup' })
 };
 
