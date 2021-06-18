79e9380012f730a27be0c7356c7ee28d072e0de9
Louis Lambeau <llambeau@quadrabee.com>
2021-06-18 16:06:51 +0200

    Funkier cmdline.

--- src/commands/list-users.js
+++ src/commands/list-users.js
@@ -6,9 +6,19 @@ const cli = require('cli-ux').default;
 class ListUsers extends Command {
   async run() {
     const config = loadConfig();
+
+    cli.action.start('Connecting to the hub');
     const client = await createClient(config);
+    cli.action.stop();
+    cli.action.start('Authenticating');
     await client.authenticate();
+    cli.action.stop();
+
+    cli.action.start('Retrieving list of users');
     const users = await client.listUsers();
+    cli.action.stop();
+
+    this.log();
     cli.table(users, {
       username: {
         minWidth: 7
